634354fb922ca9ac19051481789fd1f2f21f9e29
praiseike <praiseike123@gmail.com>
2024-03-10 23:16:08 +0100

    added passport to app service provider

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/run-migrations',function (){
+    \Artisan::call('migrate');
+    return "migrated successfully";
+});
+
+
+Route::get('/seed-database',function (){
+    \Artisan::call('db:seed');
+    return "seeded successfully";
+
+});
+
+Route::get('/install-passport',function (){
+    \Artisan::call('artisan passport:install --force');
+    return "installed successfully";
+
+});
+
+
+Route::get('/run-migrations-fresh',function (){
+    \Artisan::call('migrate:fresh --seed');
+    return "migrated successfully";
+});
+
+Route::get('/clear-cache',function(){
+    \Artisan::call('cache:clear');
+    \Artisan::call('config:clear');
+    \Artisan::call('view:clear');
+    return "cleared cache successfully";
+});
